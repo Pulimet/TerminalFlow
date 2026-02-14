1eb85f9dbad572eea5d44604d7b52ec1a81e648f
Korolev, Alexey (ak3140) <ak3140@att.com>
2026-02-14 23:00:29 +0200

    feat: define terminal flow view as webview and implement Content Security Policy

--- src/TerminalFlowProvider.ts
+++ src/TerminalFlowProvider.ts
@@ -83,6 +83,7 @@ export class TerminalFlowProvider implements vscode.WebviewViewProvider {
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource} 'unsafe-inline'; script-src 'nonce-${nonce}';">
                 <link href="${styleMainUri}" rel="stylesheet">
                 <title>Terminal Flow</title>
             </head>
